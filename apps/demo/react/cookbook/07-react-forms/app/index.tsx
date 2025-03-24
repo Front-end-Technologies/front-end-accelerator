88ea5c8111b365d276162dfd3281066280218468
Vuong Ha <vuong.ha@cegeka.com>
2025-03-24 15:31:29 +0100

    [react] Add react-hook-form with zod

--- apps/demo/react/cookbook/07-react-forms/app/index.tsx
+++ apps/demo/react/cookbook/07-react-forms/app/index.tsx
@@ -1,7 +1,83 @@
-import { useState } from 'react';
+/**
+ * @fileoverview This file contains a React component that demonstrates the use of react-hook-form
+ * with zod for form validation and type inference.
+ */
 
-export function App() {
-  const [title] = useState('Hello World');
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-  return <h1>{title}</h1>;
-}
+/**
+ * Schema definition using zod for form validation.
+ */
+const schema = z.object({
+  age: z
+    .number()
+    .positive()
+    .min(1, { message: 'Age must be a positive number and greater than 0' }),
+  name: z
+    .string()
+    .min(3, { message: 'Name should be at least 3 characters long' }),
+});
+
+/**
+ * Type inference for the schema using zod.
+ */
+type Schema = z.infer<typeof schema>;
+
+/**
+ * App component that renders a form using react-hook-form and zod for validation.
+ * - Uses `useForm` hook from react-hook-form with zodResolver for schema validation.
+ * - Handles form submission and reset.
+ * - Displays form fields, validation errors, dirty fields, and submission status.
+ */
+export const App = () => {
+  const {
+    formState: { dirtyFields, errors, isSubmitSuccessful },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
+
+  /**
+   * Handles form submission.
+   * @param data - The form data that conforms to the Schema type.
+   */
+  const onSubmit = (data: Schema) => {
+    // submit date to the server
+    console.log(data);
+    // onSucces
+    reset();
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
+      >
+        <label>
+          Name:
+          <input {...register('name')} />
+          {errors.name && <span>{errors.name.message}</span>}
+        </label>
+        <label>
+          Age:
+          <input {...register('age', { valueAsNumber: true })} type="number" />
+          {errors.age && <span>{errors.age.message}</span>}
+        </label>
+        <button type="submit">Submit</button>
+        <button onClick={() => reset()} type="button">
+          Reset
+        </button>
+
+        <strong>Dirty Fields</strong>
+        <pre>{JSON.stringify(dirtyFields, null, 2)}</pre>
+        <strong>isSubmitSuccessful</strong>
+        {isSubmitSuccessful ? 'true' : 'false'}
+      </form>
+    </div>
+  );
+};
