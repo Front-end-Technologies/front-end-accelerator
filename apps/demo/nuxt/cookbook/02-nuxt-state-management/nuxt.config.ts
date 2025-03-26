c42d9ea993e90dfc8cff0209612007eb2ced7455
Vuong Ha <vuong.ha@cegeka.com>
2025-03-26 16:39:52 +0100

    Revert

--- apps/demo/nuxt/cookbook/02-nuxt-state-management/nuxt.config.ts
+++ apps/demo/nuxt/cookbook/02-nuxt-state-management/nuxt.config.ts
@@ -3,7 +3,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
-  // SSR should be enabled, but for webcontainers, it is disabled
-  // since webcontainers do not support server-side rendering
-  ssr: false,
 });
