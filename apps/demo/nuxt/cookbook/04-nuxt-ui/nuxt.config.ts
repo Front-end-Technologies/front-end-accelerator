2723616c4220a5a62e703b37e402c6d719538627
Vuong Ha <vuong.ha@cegeka.com>
2025-03-31 17:29:14 +0200

    Remove tailwind 4 for webcontainer

--- apps/demo/nuxt/cookbook/04-nuxt-ui/nuxt.config.ts
+++ apps/demo/nuxt/cookbook/04-nuxt-ui/nuxt.config.ts
@@ -5,8 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [tailwindcss()],
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',
+          defer: true,
+        },
+      ],
+    },
   },
 });
