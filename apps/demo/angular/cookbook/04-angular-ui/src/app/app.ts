8c221cabbc482d082cfafa5d57b01f6753c1bbb3
Vuong Ha <vuong.ha@cegeka.com>
2025-09-02 22:44:36 +0200

    Move Ui into separate folder

--- apps/demo/angular/cookbook/04-angular-ui/src/app/app.ts
+++ apps/demo/angular/cookbook/04-angular-ui/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { EctzButtonModule, CardComponent, PageComponent } from './ui';
+import { EctzButtonModule, CardComponent, PageComponent } from '../ui';
 
 @Component({
   imports: [EctzButtonModule, CardComponent, PageComponent],
