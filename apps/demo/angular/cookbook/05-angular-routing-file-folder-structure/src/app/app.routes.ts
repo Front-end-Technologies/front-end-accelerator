5e808ddb463d41c9f11fd5d4468496d273d3e1dd
Vuong Ha <vuong.ha@cegeka.com>
2025-09-03 17:55:24 +0200

    Fix

--- apps/demo/angular/cookbook/05-angular-routing-file-folder-structure/src/app/app.routes.ts
+++ apps/demo/angular/cookbook/05-angular-routing-file-folder-structure/src/app/app.routes.ts
@@ -2,14 +2,9 @@ import { Routes } from '@angular/router';
 
 import { authGuard } from './guards/auth-guard';
 import { DashboardComponent } from './pages/dashboard.component';
-import { LoginComponent } from './pages/login.component';
 import { UserDetailComponent } from './pages/user.component';
 
 export const routes: Routes = [
-  {
-    component: LoginComponent,
-    path: 'login',
-  },
   {
     loadComponent: () =>
       import('./pages/about.component').then((m) => m.AboutComponent),
--- apps/demo/angular/cookbook/05-angular-routing-file-folder-structure/src/app/app.ts
+++ apps/demo/angular/cookbook/05-angular-routing-file-folder-structure/src/app/app.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { Auth } from './interfaces/auth.interface';
+import { AuthService } from './services/auth.service';
+
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   selector: 'app-root',
   template: `
     <h1>Routing</h1>
+
+    @if (credentials()) {
+      <h2>User Logged In</h2>
+      <pre>
+  {{ credentials() | json }}
+    </pre
+      >
+
+      <button (click)="logout()">Logout</button>
+    }
+
+    @if (!credentials()) {
+      <h2>Please Log in</h2>
+      <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
+        <label>
+          Username:
+          <input formControlName="username" />
+        </label>
+
+        <label>
+          Password:
+          <input type="password" formControlName="password" />
+        </label>
+
+        <button type="submit" [disabled]="loginForm.invalid">Login</button>
+      </form>
+    }
+
+    <hr />
 
     <p>This is a simple example of Angular routing</p>
 
     <nav>
       <ul>
-        <li><a routerLink="login">Login</a></li>
-        <li><a routerLink="about">About</a></li>
+        <li><a routerLink="about">About (public page, lazy-loaded)</a></li>
         <li>
           <a routerLink="dashboard"
             >Dashboard (protected page only visible after login in)</a
@@ -22,7 +60,41 @@ import { RouterModule } from '@angular/router';
     </nav>
 
     <hr />
+
     <router-outlet />
   `,
 })
-export class App {}
+export class App implements OnInit {
+  authService = inject(AuthService);
+  credentials = signal<Auth | null>(null);
+
+  loginForm = new FormGroup({
+    password: new FormControl('emilyspass', Validators.required),
+    username: new FormControl('emilys', Validators.required),
+  });
+
+  logout() {
+    this.credentials.set(null);
+    localStorage.removeItem('auth');
+  }
+
+  ngOnInit() {
+    this.logout();
+  }
+
+  async onSubmit() {
+    if (
+      this.loginForm.valid &&
+      this.loginForm.value.username &&
+      this.loginForm.value.password
+    ) {
+      const res = await this.authService.login({
+        password: this.loginForm.value.password,
+        username: this.loginForm.value.username,
+      });
+
+      this.credentials.set(res);
+      localStorage.setItem('auth', JSON.stringify(res));
+    }
+  }
+}
