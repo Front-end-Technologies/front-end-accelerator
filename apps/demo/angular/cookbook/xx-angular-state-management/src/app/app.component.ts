0c0a8d110c12859aea6b5e6ab9f595748fefe1d3
Vuong Ha <vuong.ha@cegeka.com>
2025-11-10 15:04:44 +0100

    fix

--- apps/demo/angular/cookbook/xx-angular-state-management/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: ` <h1>{{ this.title }}</h1>`,
-})
-export class AppComponent {
-  title = '01-angular-hello-world';
-}
